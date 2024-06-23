767af0010417145182f00bb197728a5de867184e
Asem Ashraf <asem.ashraf18@gmail.com>
2024-06-23 14:43:56 +0300

    control.py: adjusted weights, added braking for safe-distance keeping, formatting

--- control.py
+++ control.py
@@ -3,29 +3,32 @@
 import numpy as np
 import casadi as ca
 
+
 class MPC():
+
     def __init__(self, dynamics, Horizon, T):
         self.N = Horizon
 
         self.n_controls = dynamics.n_controls
-        self.n_states   = dynamics.n_states
+        self.n_states = dynamics.n_states
 
         self.total_states = Horizon
         self.total_controls = self.total_states - 1
 
-        self.control_values = np.zeros(self.total_controls*self.n_controls)
-        self.states_values  = np.zeros(self.total_states*self.n_states)
+        self.control_values = np.zeros(self.total_controls * self.n_controls)
+        self.states_values = np.zeros(self.total_states * self.n_states)
 
-        self.U      = ca.SX.sym('u',    self.n_controls,self.total_controls)
+        self.U = ca.SX.sym('u', self.n_controls, self.total_controls)
 
-        self.X      = ca.SX.sym('x',    self.n_states,  self.total_states)
-        self.X_ref  = ca.SX.sym('x_ref',self.n_states,  self.total_states)
+        self.X = ca.SX.sym('x', self.n_states, self.total_states)
+        self.X_ref = ca.SX.sym('x_ref', self.n_states, self.total_states)
 
-        self.UL     = ca.SX.sym('ul', self.n_controls)
+        self.UL = ca.SX.sym('ul', self.n_controls)
 
         obj, g = self.get_costfunction(T, dynamics.changeInStates)
 
-        self.lbx, self.ubx, self.lbg, self.ubg, opt_params, opt_variables = self.arrange()
+        self.lbx, self.ubx, self.lbg, self.ubg, opt_params, opt_variables = self.arrange(
+        )
 
         nlp_prob = {
             'f': obj,
@@ -34,8 +37,9 @@ class MPC():
             'g': ca.vertcat(*g)
         }
         opts_setting = {
-            'ipopt.max_iter': 100,
+            'ipopt.max_iter': 50,
             'ipopt.print_level': 0,
+            'print_time': 0,
             'ipopt.acceptable_tol': 1e-8,
             'ipopt.acceptable_obj_change_tol': 1e-6
         }
@@ -44,8 +48,10 @@ class MPC():
     def get_control(self, references):
         N = self.N
         all_states_init_val = np.array(references[:N]).reshape(-1)
-        parameters = np.concatenate((self.control_values[:2], all_states_init_val))
-        init_values = np.concatenate((self.control_values, all_states_init_val))
+        parameters = np.concatenate(
+            (self.control_values[:2], all_states_init_val))
+        init_values = np.concatenate(
+            (self.control_values, all_states_init_val))
         solution = self.solver(x0=init_values,
                                p=parameters,
                                lbx=self.lbx,
@@ -54,37 +60,54 @@ class MPC():
                                ubg=self.ubg)
 
         estimated_values = solution['x'].full()
-        self.control_values = estimated_values[:self.n_controls*(self.total_controls)].reshape(-1)
-        self.states_values  = estimated_values[(self.total_controls)*self.n_controls:].reshape(-1)
-        acc = estimated_values[0][0]/self.a_max
-        steer = estimated_values[1][0]/self.delta_max
+        self.control_values = estimated_values[:self.n_controls *
+                                               (self.total_controls)].reshape(
+                                                   -1)
+        self.states_values = estimated_values[(self.total_controls) *
+                                              self.n_controls:].reshape(-1)
+        acc = estimated_values[0][0] / self.a_max
+        steer = estimated_values[1][0] / self.delta_max
+
+        dis = 0
+        # for i in range(N-1):
+        x1, y1 = references[0][0], references[0][1]
+        x2, y2 = references[-1][0], references[-1][1]
+        dis += np.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
+        print(chr(27) + "[2J")
+        print("Distance = ", dis)
+        if dis < 12:
+            # self.control_values[0] = -1
+            print("========================================")
+            return -1, steer
+
         return acc, steer
 
     def arrange(self):
-        self.a_max = 6
-        self.delta_max = np.deg2rad(70)
+        self.a_max = 60
+        self.delta_max = np.deg2rad(150)
         upper_bounds_states = {
             'x': np.inf,
             'y': np.inf,
-            'psi': np.pi,
-            'vx': 30, # the limit below which carla max acceleration = 6
-            'vy': 3,
-            'psi_dot': 2,
+            'psi': np.inf,
+            'vx': 30,  # the limit below which carla max acceleration = 6
+            'vy': np.inf,
+            'psi_dot': np.inf,
         }
         lower_bounds_states = {
             'x': -np.inf,
             'y': -np.inf,
-            'psi': -np.pi,
-            'vx': 0, # can't go backwards
-            'vy': -3,
-            'psi_dot': -2,
+            'psi': -np.inf,
+            'vx': 0,  # can't go backwards
+            'vy': -np.inf,
+            'psi_dot': -np.inf,
         }
         upper_bounds_controls = {
             'acc': self.a_max,
             'delta': self.delta_max,
         }
         lower_bounds_controls = {
-            'acc': -self.a_max, # This negative value is not for going backward (braking).
+            'acc': -self.
+            a_max,  # This negative value is not for going backward (braking).
             'delta': -self.delta_max,
         }
         lbs = list(lower_bounds_states.values())
@@ -105,14 +128,9 @@ class MPC():
             lbx = np.concatenate((lbx, lbs))
             ubx = np.concatenate((ubx, ubs))
 
-        opt_variables = ca.vertcat(
-                ca.reshape(self.U, -1, 1),
-                ca.reshape(self.X, -1, 1)
-                )
-        opt_params = ca.vertcat(
-                self.UL,
-                ca.reshape(self.X_ref, -1, 1)
-                )
+        opt_variables = ca.vertcat(ca.reshape(self.U, -1, 1),
+                                   ca.reshape(self.X, -1, 1))
+        opt_params = ca.vertcat(self.UL, ca.reshape(self.X_ref, -1, 1))
 
         return lbx, ubx, lbg, ubg, opt_params, opt_variables
 
@@ -120,39 +138,47 @@ class MPC():
         g = []
         obj = 0
 
-        # Minimize     X,   Y,psi,Vx,Vy,omega
-        Q = np.diag([10, 10, 100, 5, 5, 1])
+        # Minimize
+        Q = np.diag([
+            400,  # X
+            400,  # Y
+            400,  # psi
+            10,  # Vx
+            1,  # Vy
+            1  # omega
+        ])
 
-        # Minimize change in acceleration and steering
-        Rchange = np.diag([9, 9])
-        # Minimize acceleration
-        R = np.diag([1, 0.0])
+        # Minimize change in acceeration and steering
+        Rchange = np.diag([0.01, 0.1])
+        # Minimize acceeration
+        R = np.diag([0, 0.0])
 
         g.append(self.X[:, 0] - self.X_ref[:, 0])
         for i in range(self.n_states - 1):
-            nextstate = self.X[:, i] + t * changeinstates( self.X[:, i], self.U[:, i])
+            nextstate = self.X[:, i] + t * changeinstates(
+                self.X[:, i], self.U[:, i])
             g.append(self.X[:, i + 1] - nextstate)
 
-
         # minimize error in references for better following of the trajectory
-        for i in range(self.n_states-1):
+        for i in range(self.n_states - 1):
             referror = self.X[:, i] - self.X_ref[:, i]
             obj = obj + ca.mtimes([referror.T, Q, referror])
         # prioritize minimize error in the last references point for better following of the trajectory
         referror = self.X[:, -1] - self.X_ref[:, -1]
-        obj = obj + ca.mtimes([referror.T, 2*Q, referror])
+        obj = obj + ca.mtimes([referror.T, 2 * Q, referror])
 
         # minimize controls for more fuel efficiency(if the acc was reduced)
-        for i in range(self.n_controls):
-            cont = self.U[:, i]
-            obj = obj + ca.mtimes( [cont.T, R, cont])
+        # for i in range(self.n_controls):
+        #     cont = self.U[:, i]
+        #     obj = obj + ca.mtimes([cont.T, R, cont])
 
         # given the control that was applied last time, minimize the difference between the last control and the first control
         changeincont = self.U[:, 0] - self.UL
-        obj = obj + ca.mtimes([ changeincont.T, Rchange, changeincont ])
+        obj = obj + ca.mtimes([changeincont.T, Rchange, changeincont])
         # Then minimize control changes across the whole trajectory for more vehicle stability
         for i in range(self.n_controls - 1):
             changeincont = self.U[:, i] - self.U[:, i + 1]
-            obj = obj + ca.mtimes([ changeincont.T, 0.1 * Rchange, changeincont ])
+            obj = obj + ca.mtimes(
+                [changeincont.T, 0.1 * Rchange, changeincont])
 
         return obj, g
