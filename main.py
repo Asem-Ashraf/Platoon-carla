main.py: fixed 360 overflow problem, converted the simulation to synchronous mode, and added cameras to capture the platoon.

main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3.7
 
 import carla
-import time
 import numpy as np
-import threading
+import cv2
 
 from world import World
 from arguments import parseArguments
@@ -12,10 +11,6 @@ import fusion as fu
 import equations as eq
 import control as con
 
-test = 0
-refs = []
-N = 15
-Ts = 0.08
 
 def main():
     np.set_printoptions(suppress=True,precision=4,linewidth=200)
@@ -26,74 +21,152 @@ def main():
 
     bp = sim_world.bp
 
-    spawns = sim_world.map.get_spawn_points()
-    transform = spawns[221]
+    refs = []
+    N = 80
+    Ts = 0.02
 
-    cars = []
+    # spawns = sim_world.map.get_spawn_points()
+    # transform = spawns[222]
+    # transform = spawns[13]
+    transform = carla.Transform(carla.Location(x=72.432770, y=-7.948537, z=0.1), carla.Rotation(pitch=1.276521, yaw=-62.710632, roll=0.036662))
+
+    actors = []
 
     try:
 
+        # spawn a leader vehicle
         leader   = sim_world.world.spawn_actor(bp, transform)
-        cars.append(leader)
+        actors.append(leader)
+
+        # move the spectator to a top view above the leader (it does NOT move with the leader)
+        spec_trans = carla.Transform()
+        spec_trans.location = transform.location
+        spec_trans.rotation = transform.rotation
+        spec_trans.location.z += 30
+        spec_trans.rotation.pitch-=90
+        spec_trans.rotation.yaw+=90
+        sim_world.spectator.set_transform(spec_trans)
+
+        # a camera blueprint. This camera blueprint is used twice to record a video of the platoon from various angels.
+        camera_bp = sim_world.blueprint_lib.filter('sensor.camera.rgb')[0]
+
+        # FHD video
+        height, width = 1080, 1920
+        # 50 fps
+        cameraFPS = 50
+        frametime = 1/cameraFPS
+
+        camera_bp.set_attribute('image_size_x', str(width))  # Set width
+        camera_bp.set_attribute('image_size_y', str(height))   # Set height
+        camera_bp.set_attribute('sensor_tick', str(frametime)) # Set the time in seconds between sensor captures
+
+        # Front view angle transform
+        frontViewTrans = carla.Transform(carla.Location(4.5,-2.5,1.5),carla.Rotation(0,20,0))
+
+        # Top view angle transform
+        topViewTrans = carla.Transform(carla.Location(-10,0,30),carla.Rotation(-30,0,90))
+
+        # Attaching the camera to the leader
+        frontViewCamActor = sim_world.world.spawn_actor(
+                camera_bp,
+                frontViewTrans,
+                attach_to=leader,
+                # SpringArm attachment for less shaky videos
+                attachment_type=carla.AttachmentType.SpringArm)
+        actors.append(frontViewCamActor)
+
+        # Attaching the camera to the leader
+        topViewCamActor = sim_world.world.spawn_actor(
+                camera_bp,
+                topViewTrans,
+                attach_to=leader,
+                # SpringArm attachment for less shaky videos
+                attachment_type=carla.AttachmentType.SpringArm)
+        actors.append(topViewCamActor)
+
+        frontViewVideo = cv2.VideoWriter('FrontView.avi', cv2.VideoWriter_fourcc(*'XVID'), cameraFPS, (width, height))
+        topViewVideo = cv2.VideoWriter('TopView.avi', cv2.VideoWriter_fourcc(*'XVID'), cameraFPS, (width, height))
+
+        def addImageToVid(video,image):
+            # Convert the raw image data to a numpy array
+            array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+            array = np.reshape(array, (image.height, image.width, 4))
+            # Convert from BGRA to BGR
+            bgr_array = array[:, :, :3]
+            # Store the image in the list
+            video.write(bgr_array)
+        # Set up the camera to call the process_image function for each captured frame
+
+        frontViewCamActor.listen(lambda image: addImageToVid(frontViewVideo,image))
+        topViewCamActor.listen(lambda image: addImageToVid(topViewVideo,image))
+
         leader.set_autopilot()
-        sim_world.tm.ignore_signs_percentage(leader,100)
+
+
+
 
         global refs
-        for i in range(N*3):
+        refs.append(fu.get_current_state(leader))
+        print(sim_world.world.tick())
+        for i in range(N):
             refs.append(fu.get_current_state(leader))
-            # print(refs[-1])
-            time.sleep(Ts)
+            print("before: ",refs[-1][2])
+            if (refs[-2][2]-refs[-1][2])>np.pi:
+                refs[-1][2]+=2*np.pi
+            elif (refs[-2][2]-refs[-1][2])<-np.pi:
+                refs[-1][2]-=2*np.pi
+            print("after: ",refs[-1][2])
+            print(sim_world.world.tick())
+
 
         follower = sim_world.world.spawn_actor(bp, transform)
-        cars.append(follower)
-        vehicle = con.MPC(eq.VehicleModel(),N,Ts)
-        # vehicle = con.MPC(eq.VehicleModel(follower),N,Ts)
+        actors.append(follower)
 
-        def move(leader,follower):
-            while True:
-                refs.pop(0)
-                refs[0] = fu.get_current_state(follower)
-                refs.append(fu.get_current_state(leader))
-                time.sleep(Ts)
+        print(sim_world.world.tick())
+        print(sim_world.world.tick())
 
-        t1 = threading.Thread(target=move,args=(leader,follower))
-        t1.start()
-
+        # vehicle = con.MPC(eq.VehicleModel(),N,Ts)
+        vehicle = con.MPC(eq.VehicleModel(follower),N,Ts)
+        index = 0
         while True:
-            start = time.time()
-            # referencesss = refs[:N+1]
-            # # if the distance between the 0th state and the Nth state is less than distance D
-            # # then the velocity should be all zeros in these states
-            # dis = 0
-            # for i in range(N):
-            #     dis += np.sqrt(((referencesss[i][0]-referencesss[i+1][0])**2)+((referencesss[i][1]-referencesss[i+1][1])**2))
-            # print(dis)
-            # if(dis<=7):
-            #     # print(referencesss)
-            #     for i in range(N):
-            #         referencesss[i+1][3] = 0
-            acc, steer = vehicle.get_control(refs)
-            end = time.time()
+            refs.append(fu.get_current_state(leader))
+
+            differ =refs[-2][2]-refs[-1][2]
+            times = round(abs(differ/(2*np.pi)))
+            if (differ)>np.pi:
+                refs[-1][2]+=2*np.pi*times
+            elif (differ)<-np.pi:
+                refs[-1][2]-=2*np.pi*times
+
+            follower_list = refs[-N:]
+            follower_list[0]=fu.get_current_state(follower)
+
+            differ = follower_list[0][2]-follower_list[1][2]
+            times = round(abs(differ/(2*np.pi)))
+            if (differ)<-np.pi:
+                follower_list[0][2]+=2*np.pi*times
+            elif (differ)>np.pi:
+                follower_list[0][2]-=2*np.pi*times
+
+            acc, steer = vehicle.get_control(follower_list)
             if (acc < 0.0):
                 brake = abs(acc)
                 acc = 0.0
             else:
                 brake = 0.0
-            follower.apply_control(carla.VehicleControl(throttle=acc, steer=steer,brake = brake))
-            print("-----------------------")
             print("a ",acc)
             print("d ",steer)
             print("b ",brake)
-            print("solver time: ",(end-start)*1000,"ms")
-            # print("-----------------------")
-            delay = Ts-(end-start)
-            if(delay>0):
-                time.sleep(delay)
+            index += 1
+            follower.apply_control(carla.VehicleControl(throttle=acc, steer=steer,brake = brake))
+            sim_world.world.tick()
 
     finally:
-        for car in cars:
+        for car in actors:
             car.destroy()
-        print('#############')
+        topViewVideo.release()
+        frontViewVideo.release()
+        print('\n#############')
         print('# Destroyed #')
         print('#############')
 
@@ -105,15 +178,3 @@ if __name__ == '__main__':
         print('\nCancelled by the user. Bye!')
     except RuntimeError as e:
         print(e)
-# print("where I got to           ",np.array(refs[0]))
-# print("apply this to go                 +",u[0])
-# print("where I want to go next  ",np.array(refs[1]))
-# print("I think I will get there ", st[1])
-# sum = [0,0,0,0,0,0]
-# for i in range(N-1):
-#     print("ref ",i," ",np.array(refs[i]))
-#     print("inp ",i,"    +  ",u[i])
-#     print("sta ",i," ",st[i])
-#     print("err ",i," ",st[i]-np.array(refs[i]))
-#     print()
-#     sum += refs[i]-st[i]