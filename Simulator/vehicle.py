6efdfd189da6058a84b18f3ebe8821c5e1d5ec24
Asem Ashraf <asem.ashraf18@gmail.com>
2024-06-25 02:18:03 +0300

    merged fusion.py into vehicle.py

--- Simulator/vehicle.py
+++ Simulator/vehicle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.7
 
-import fusion as fu
+import numpy as np
 
 
 class Vehicle:
@@ -10,7 +10,48 @@ class Vehicle:
         self.carla_instance = vehicle_actor_instance
 
     def getData(self):
-        return fu.get_current_state(self.carla_instance)
+        '''
+        X     - Global x position (meter)
+        Y     - Global y position (meter)
+        psi   - Heading angle (radians)
+        Vx    - Longitudinal velocity (m/s)
+        Vy    - Lateral velocity (m/s)
+        omega - Angular velocity (rad/s)
+
+        return: state - the state at the next time step
+        '''
+        transform = self.carla_instance.get_transform()
+        # position in Global coordinates
+        X = transform.location.x
+        Y = transform.location.y
+
+        # yaw in degrees
+        psi = transform.rotation.yaw % 360
+        # Limit between ]-pi,pi]
+        if psi > 180.0:
+            psi = psi - 360.0
+        if psi <= -180.0:
+            psi = psi + 360.0
+        psi = np.deg2rad(psi)
+
+        # velocity in global coordinates
+        v = self.carla_instance.get_velocity()
+
+        # velocities in vehicle coordinates
+        Vx = v.x*np.cos(psi) + v.y*np.sin(psi)
+        Vy = -v.x*np.sin(psi) + v.y*np.cos(psi)
+
+        omega = np.deg2rad(self.carla_instance.get_angular_velocity().z)
+
+        next_state = [
+            X,
+            Y,
+            psi,
+            Vx,
+            Vy,
+            omega
+        ]
+        return np.array(next_state)
 
     def destroy(self):
         self.carla_instance.destroy()
